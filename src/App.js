44af95ae87f1c9695154d78dea48894f4735836d
Menna Hamdy <menna.h.m.elsayed@gmail.com>
2022-04-11 00:08:52 +0200

    version 4

--- src/App.js
+++ src/App.js
@@ -1,162 +1,160 @@
 import { useState, useEffect } from "react";
+import "./App.css";
+export function TodoInput({ onAdd }) {
+  let [value, setValue] = useState("");
+  return (
+    <div class="input">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
 
-export function TodoInput({ onAdd }){
-
-let [value, setValue] = useState("");
-return (<div >
-    <input 
-    type = "text"
-    value={value}
-    onChange={(e)=>{
-        setValue(e.target.value);
-    }
-
-    }/>
-    
-<button
-onClick={()=>{
-    onAdd(value);
-    setValue("");
-}}
->Add item</button>
-</div>
-
-
-);
-
+      <button
+        onClick={() => {
+          onAdd(value);
+          setValue("");
+        }}
+      >
+        Add item
+      </button>
+    </div>
+  );
 }
-export function TodoList({ items,onDelete,onCheck,onDoubleClick, onSave }){
-    return (
-    
-    
+export function TodoList({ items, onDelete, onCheck, onDoubleClick, onSave }) {
+  return (
     <ul>
-{items.map((item)=>(
-<TodoItem
-value= {item}
-onDelete={()=>{
-onDelete(item);
-    
-
-}}
-onCheck={()=>{
-    onCheck(item);
-    
-    }}
-onDoubleClick={(setEditing)=>{
-    onDoubleClick(setEditing);
-}
-}
-onSave ={(edited,setEditing)=>{
-    onSave(edited,setEditing,item);
-}}
-/>
-
-
-))}
-
-</ul>
-);
+      {items.map((item) => (
+        <TodoItem
+          value={item}
+          onDelete={() => {
+            onDelete(item);
+          }}
+          onCheck={() => {
+            onCheck(item);
+          }}
+          onDoubleClick={(setEditing) => {
+            onDoubleClick(setEditing);
+          }}
+          onSave={(edited, setEditing) => {
+            onSave(edited, setEditing, item);
+          }}
+        />
+      ))}
+    </ul>
+  );
 }
 
-export function TodoItem({ value, onDelete,onCheck,onDoubleClick,onSave }){
-    let [edited, setValue] = useState(value.title);
-       
-       const [editing,setEditing] = useState(false);
+export function TodoItem({ value, onDelete, onCheck, onDoubleClick, onSave }) {
+  let [edited, setValue] = useState(value.title);
 
-      let viewMode = {}
-      let editMode = {}
-      
-      if (editing) {
-        viewMode.display = "none"
-      } else {
-        editMode.display = "none"
-      }
-    return (
+  const [editing, setEditing] = useState(false);
+
+  let viewMode = {};
+  let editMode = {};
+
+  if (editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+  return (
     <>
-    <li style={viewMode}>
-        <input 
-        type="checkbox" 
-        checked={value.completed} 
-        onChange={
-            ()=>{onCheck()
-        }
-        }
+      <li style={viewMode}>
+        <input
+          type="checkbox"
+          checked={value.completed}
+          onChange={() => {
+            onCheck();
+          }}
         />
 
-        <span onDoubleClick={()=>{onDoubleClick(setEditing);}}>{value.title}</span>
-        <button onClick={()=>{onDelete()}}>X</button>
-
-    </li>
-    <li style={editMode}>
-<input type="text" value={edited} 
-
-onChange={(e)=>{
-    setValue(e.target.value);
-}
-
-}
-/>
-<button 
-onClick={()=>{
-    onSave(edited,setEditing);
-    setValue("");
-}}
->Save</button>
-</li>
+        <span
+          onDoubleClick={() => {
+            onDoubleClick(setEditing);
+          }}
+        >
+          {value.title}
+        </span>
+        <button
+          onClick={() => {
+            onDelete();
+          }}
+        >
+          X
+        </button>
+      </li>
+      <li style={editMode}>
+        <input
+          type="text"
+          value={edited}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            onSave(edited, setEditing);
+            setValue("");
+          }}
+        >
+          Save
+        </button>
+      </li>
     </>
-);
+  );
 }
 
-export function App(){
-const [items, setItems]= useState([
-    {id: 1, title: "Item1", completed: true },
-    {id: 2, title: "Item2", completed: false }
-]);
-useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then((json) => setItems(json));
+export function App() {
+  const [items, setItems] = useState([
+    { id: 1, title: "Item1", completed: true },
+    { id: 2, title: "Item2", completed: false },
+  ]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setItems(json));
   }, []);
-return (<div>
-    <TodoInput onAdd={(value) => {
-        setItems([
+  return (
+    <div class='App'>
+      <TodoInput
+        onAdd={(value) => {
+          setItems([
             ...items,
-            {id: Math.random(), label: value, completed: false }
-        ]);
-    }
-    }
-    />
-    <TodoList items={items}
-    onDelete={(item)=>{
-        setItems(items.filter((todoItem)=>todoItem.id !== item.id));
-    }
-
-}
- onCheck = {(item) =>  {
-    
-    let newArr = [...items]; 
-    for(let i =0; i<newArr.length;i++){
-    if(newArr[i]==item){
-        newArr[i].completed = !newArr[i].completed;
-    }
-    }
-    
-    setItems(newArr);
-  }
-}
-    onDoubleClick = {(setEditing) => {
-        
-        setEditing(true);
-
-      }}
-      onSave ={(edited,setEditing,item) => {
-        setItems(items.map((todoItem)=>todoItem.id === item.id ? {... todoItem, title : edited}: todoItem));
-        setEditing(false);
-    }
-    }
-
-/>
-</div>
-);
-
+            { id: Math.random(), label: value, completed: false },
+          ]);
+        }}
+      />
+      <TodoList
+        items={items}
+        onDelete={(item) => {
+          setItems(items.filter((todoItem) => todoItem.id !== item.id));
+        }}
+        onCheck={(item) => {
+          setItems(
+            items.map((todoItem) =>
+              todoItem.id === item.id
+                ? { ...todoItem, completed: !todoItem.completed }
+                : todoItem
+            )
+          );
+        }}
+        onDoubleClick={(setEditing) => {
+          setEditing(true);
+        }}
+        onSave={(edited, setEditing, item) => {
+          setItems(
+            items.map((todoItem) =>
+              todoItem.id === item.id
+                ? { ...todoItem, title: edited }
+                : todoItem
+            )
+          );
+          setEditing(false);
+        }}
+      />
+    </div>
+  );
 }
