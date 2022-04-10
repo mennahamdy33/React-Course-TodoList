5de29fbf777b370c379cdf9509a1f2b9442c7cf9
Menna Hamdy <menna.h.m.elsayed@gmail.com>
2022-04-10 09:40:42 +0200

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function TodoInput({ onAdd }){
+
+let [value, setValue] = useState("");
+return (<div >
+    <input 
+    type = "text"
+    value={value}
+    onChange={(e)=>{
+        setValue(e.target.value);
+    }
+
+    }/>
+    
+<button
+onClick={()=>{
+    onAdd(value);
+    setValue("");
+}}
+>Add item</button>
+</div>
+
+
+);
+
+}
+export function TodoList({ items,onDelete,onCheck,onDoubleClick }){
+    return (
+    
+    
+    <ul>
+{items.map((item)=>(
+<TodoItem
+value= {item}
+onDelete={()=>{
+onDelete(item);
+    
+
+}}
+onCheck={()=>{
+    onCheck(item);
+    
+    }}
+onDoubleClick={(setEditing)=>{
+    onDoubleClick(setEditing);
+}
+}
+/>
+
+
+))}
+
+</ul>
+);
 }
 
-export default App;
+export function TodoItem({ value, onDelete,onCheck,onDoubleClick }){
+    
+       
+       const [editing,setEditing] = useState(false);
+
+      let viewMode = {}
+      let editMode = {}
+      
+      if (editing) {
+        viewMode.display = "none"
+      } else {
+        editMode.display = "none"
+      }
+    return (
+    <>
+    <li style={viewMode}>
+        <input 
+        type="checkbox" 
+        checked={value.completed} 
+        onChange={
+            ()=>{onCheck()
+        }
+        }
+        />
+
+        <span onDoubleClick={()=>{onDoubleClick(setEditing);}}>{value.title}</span>
+        <button onClick={()=>{onDelete()}}>X</button>
+
+    </li>
+    <li style={editMode}>
+<input type="text"  />
+<button >Save</button>
+</li>
+    </>
+);
+}
+
+export function App(){
+const [items, setItems]= useState([
+    {id: 1, title: "Item1", completed: true },
+    {id: 2, title: "Item2", completed: false }
+]);
+useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then((json) => setItems(json));
+  }, []);
+return (<div>
+    <TodoInput onAdd={(value) => {
+        setItems([
+            ...items,
+            {id: Math.random(), label: value, completed: false }
+        ]);
+    }
+    }
+    />
+    <TodoList items={items}
+    onDelete={(item)=>{
+        setItems(items.filter((todoItem)=>todoItem.id !== item.id));
+    }
+
+}
+ onCheck = {(item) =>  {
+    
+    let newArr = [...items]; 
+    for(let i =0; i<newArr.length;i++){
+    if(newArr[i]==item){
+        newArr[i].completed = !newArr[i].completed;
+    }
+    }
+    
+    setItems(newArr);
+  }
+}
+    onDoubleClick = {(setEditing) => {
+        
+        setEditing(true);
+
+      }}
+
+/>
+</div>
+);
+
+}
